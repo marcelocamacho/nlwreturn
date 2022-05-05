e464be6a80bf0a630f3fdf600c181f95c9c4e700
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-05-05 15:57:28 -0300

    Stage 2

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        brand:{
+      colors: {
+        brand: {
+          300: '#996DFF',
           500: '#8257e6',
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
